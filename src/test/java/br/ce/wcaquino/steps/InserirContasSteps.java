a9d65d807531aee0ab6c418fb50144b36f6df79f
Gabrielle <96845972+GabiMarximo@users.noreply.github.com>
2021-12-29 20:22:51 -0300

    Update InserirContasSteps.java

--- src/test/java/br/ce/wcaquino/steps/InserirContasSteps.java
+++ src/test/java/br/ce/wcaquino/steps/InserirContasSteps.java
@@ -13,82 +13,28 @@ import io.cucumber.java.pt.Quando;
 public class InserirContasSteps {
 
 	private WebDriver driver;
-
-	@Dado("^que estou acessando a aplicação$")
-	public void queEstouAcessandoAAplicação() throws Throwable {
+	
+	@Dado(^que desejo adicionar uma conta$")
+	public void queDesejoAdicionarUmaConta() throws Throwable {
 		driver = new ChromeDriver();
 		driver.get("https://srbarriga.herokuapp.com");
-	}
-
-	@Quando("^informo o usuário \"([^\"]*)\"$")
-	public void informoOUsuário(String arg1) throws Throwable {
-		driver.findElement(By.id("email")).sendKeys(arg1);
-	}
-
-	@Quando("^a senha \"([^\"]*)\"$")
-	public void aSenha(String arg1) throws Throwable {
-		driver.findElement(By.name("senha")).sendKeys(arg1);
-	}
-
-	@Quando("^seleciono entrar$")
-	public void selecionoEntrar() throws Throwable {
+		driver.findElement(By.id("email")).sendKeys("a@c");
+		driver.findElement(By.name("senha")).sendKeys("abc");
 		driver.findElement(By.tagName("button")).click();
-	}
-
-	@Então("^visualizo a página inicial$")
-	public void visualizoAPáginaInicial() throws Throwable {
-		String texto = driver.findElement(By.xpath("//div[@class='alert alert-success']")).getText();
-		Assert.assertEquals("Bem vindo, Gabrielle!", texto);
-	}
-
-	@Quando("^seleciono Contas$")
-	public void selecionoContas() throws Throwable {
 		driver.findElement(By.linkText("Contas")).click();
+		driver.findElement(By.linkText("Adicionar"))click();
 	}
-
-	@Quando("^selecioco Adicionar$")
-	public void selecionoAdicionar() throws Throwable {
-		driver.findElement(By.linkText("Adicionar")).click();
-
-	}
-
-	@Quando("^informo a conta \"([^\"]*)\"$")
-	public void informoAConta(String arg1) throws Throwable {
+	      
+	@Quando("^adiciono a conta \"([^\"]*)\"$")
+	public void adicionoAConta(String arg1) throws Throwable {
 		driver.findElement(By.id("nome")).sendKeys(arg1);
-	}
-
-	@Quando("^seleciono Salvar$")
-	public void selecionoSalvar() throws Throwable {
 		driver.findElement(By.tagName("button")).click();
-	}
-
-	@Então("^a conta é inserida com sucesso$")
-	public void aContaÉInseridaComSucesso() throws Throwable {
-		String texto = driver.findElement(By.xpath("//div[@class='alert alert-success']")).getText();
-		Assert.assertEquals("Conta adicionada com sucesso!", texto);
-	}
-
-	@Então("^sou notificado que o nome da conta é obrigatório$")
-	public void souNotificadoQueONomeDaContaÉObrigatório() throws Throwable {
-		String texto = driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText();
-		Assert.assertEquals("Informe o nome da conta", texto);
-	}
-	
-	@Então("^sou notificado que já existe uma conta com esse nome$")
-	public void souNotificadoQueJáExisteUmaContaComEsseNome() throws Throwable {
-		String texto = driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText();
-		Assert.assertEquals("Já existe uma conta com esse nome!", texto);
 	}
 	
 	@Então("^recebo a mensagem \"([^\"]*)\"$")
 	public void receboAMensagem(String arg1) throws Throwable {
 		String texto = driver.findElement(By.xpath("//div[starts-with(@class, 'alert alert-')]")).getText();
 		Assert.assertEquals(arg1, texto);
-	}
-	
-	@Before
-	public void inicio() {
-		System.out.println("Começando aqui");
 	}
 	
 	@After(order = 1, value = {"@funcionais"})
