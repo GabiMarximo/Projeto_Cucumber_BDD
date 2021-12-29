1dab923d5e84ea82dcdb4115e034654832e58dae
GabiMarximo <96845972+GabiMarximo@users.noreply.github.com>
2021-12-29 18:48:24 -0300

    Add files via upload

---
+++ src/test/java/br/ce/wcaquino/steps/InserirContasSteps.java
@@ -0,0 +1,88 @@
+package br.ce.wcaquino.steps;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class InserirContasSteps {
+
+	private WebDriver driver;
+
+	@Dado("^que estou acessando a aplicação$")
+	public void queEstouAcessandoAAplicação() throws Throwable {
+		driver = new ChromeDriver();
+		driver.get("https://srbarriga.herokuapp.com");
+	}
+
+	@Quando("^informo o usuário \"([^\"]*)\"$")
+	public void informoOUsuário(String arg1) throws Throwable {
+		driver.findElement(By.id("email")).sendKeys(arg1);
+	}
+
+	@Quando("^a senha \"([^\"]*)\"$")
+	public void aSenha(String arg1) throws Throwable {
+		driver.findElement(By.name("senha")).sendKeys(arg1);
+	}
+
+	@Quando("^seleciono entrar$")
+	public void selecionoEntrar() throws Throwable {
+		driver.findElement(By.tagName("button")).click();
+	}
+
+	@Então("^visualizo a página inicial$")
+	public void visualizoAPáginaInicial() throws Throwable {
+		String texto = driver.findElement(By.xpath("//div[@class='alert alert-success']")).getText();
+		Assert.assertEquals("Bem vindo, Gabrielle!", texto);
+	}
+
+	@Quando("^seleciono Contas$")
+	public void selecionoContas() throws Throwable {
+		driver.findElement(By.linkText("Contas")).click();
+	}
+
+	@Quando("^selecioco Adicionar$")
+	public void selecionoAdicionar() throws Throwable {
+		driver.findElement(By.linkText("Adicionar")).click();
+
+	}
+
+	@Quando("^informo a conta \"([^\"]*)\"$")
+	public void informoAConta(String arg1) throws Throwable {
+		driver.findElement(By.id("nome")).sendKeys(arg1);
+	}
+
+	@Quando("^seleciono Salvar$")
+	public void selecionoSalvar() throws Throwable {
+		driver.findElement(By.tagName("button")).click();
+	}
+
+	@Então("^a conta é inserida com sucesso$")
+	public void aContaÉInseridaComSucesso() throws Throwable {
+		String texto = driver.findElement(By.xpath("//div[@class='alert alert-success']")).getText();
+		Assert.assertEquals("Conta adicionada com sucesso!", texto);
+	}
+
+	@Então("^sou notificado que o nome da conta é obrigatório$")
+	public void souNotificadoQueONomeDaContaÉObrigatório() throws Throwable {
+		String texto = driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText();
+		Assert.assertEquals("Informe o nome da conta", texto);
+	}
+	
+	@Então("^sou notificado que já existe uma conta com esse nome$")
+	public void souNotificadoQueJáExisteUmaContaComEsseNome() throws Throwable {
+		String texto = driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText();
+		Assert.assertEquals("Já existe uma conta com esse nome!", texto);
+	}
+	
+	@After
+	public void fecharBrowser() {
+		driver.quit();
+	}
+
+}
